d280da3dcbebc57ea5804d089df5f27ba728df4d
Lukianov Artur <artur.lukyanov@gmail.com>
2019-11-20 15:55:06 +0300

    Core file structure created, core/objects/item.py added, core/mechanics/stats.py added

---
+++ core/objects/item.py
@@ -0,0 +1,10 @@
+from ..mechanics.stats import Stats
+
+class BaseItem:
+	def __init__(self, stats=Stats(), name="Unnamed"):
+		self.stats = stats
+		self.name = name
+	
+	def __str__(self):
+		return "[BaseItem] (%s) %s" % (self.name,
+										str(self.stats))
