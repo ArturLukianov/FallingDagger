0b3b47be54a488c2604621e230abd08946ec0a2e
Lukianov Artur <artur.lukyanov@gmail.com>
2019-11-20 16:10:48 +0300

    weapon.py, sword.py added item.py -> base_item.py

--- core/objects/base_item.py
+++ core/objects/base_item.py
@@ -6,5 +6,6 @@ class BaseItem:
 		self.name = name
 	
 	def __str__(self):
-		return "[BaseItem] (%s) %s" % (self.name,
-										str(self.stats))
+		return "[%s] (%s) %s" % (type(self),
+								self.name,
+								str(self.stats))
